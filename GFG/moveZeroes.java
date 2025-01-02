f5d574084ca2f3ce34d0ecb054647982253e33da
Siddharth Singh <siddhu33singh@gmail.com>
2025-01-02 23:16:25 +0530

    GFG

---
+++ GFG/moveZeroes.java
@@ -0,0 +1,19 @@
+// GFG
+// move Zeroes
+import java.util.Arrays;
+
+public class moveZeroes {
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 0, 1, 0, 0, 1 };
+        int left = 0;
+        for(int rigth=0;rigth<arr.length;rigth++) {
+            if(arr[rigth]!=0){
+                int temp = arr[rigth];
+                arr[rigth] = arr[left];
+                arr[left]=temp;
+                left++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ GFG/printanagramsTogether.java
@@ -0,0 +1,33 @@
+//GFG
+// Print Anagrams Together
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class printanagramsTogether {
+    public static void main(String[] args) {
+        String[] arr = { "act", "god", "cat", "dog", "tac" };
+        HashMap<String, List<String>> dic = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            char[] tmp = arr[i].toCharArray();
+            Arrays.sort(tmp);
+            String tempString = new String(tmp);
+            if(dic.containsKey(tmp)){
+                List<String> anagrams = dic.get(tmp);
+                anagrams.add(arr[i]);
+                dic.put(tempString, anagrams);
+            }else{
+                List<String> anagrams = new ArrayList<>();
+                anagrams.add(arr[i]);
+                dic.put(tempString, anagrams);
+            }
+            List<List<String>> result = new ArrayList<>();
+            for(String s: dic.keySet()){
+                result.add(dic.get(s));
+            }
+            // System.out.println(result);
+        }
+    }
+}
