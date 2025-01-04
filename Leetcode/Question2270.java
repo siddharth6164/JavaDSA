5336f8abb40bcc5574555baedb33961d1571a301
Siddharth Singh <siddhu33singh@gmail.com>
2025-01-04 22:51:18 +0530

    2270. Number of Ways to Split Array

--- Leetcode/Question2270.java
+++ Leetcode/Question2270.java
@@ -1,18 +1,18 @@
 // Leetcode
 // 2270. Number of Ways to Split Array
 public class Question2270 {
-    public static void main(String[] args){
-        int[] arr={10,4,-8,7};
+    public static void main(String[] args) {
+        int[] arr = { 10, 4, -8, 7 };
         int n = arr.length;
         long totalSum = 0;
         int count = 0;
-        for(int nums:arr){
+        for (int nums : arr) {
             totalSum += nums;
         }
         long prefixSum = 0;
-        for(int i=0; i<n-1;i++){
-            prefixSum+=arr[i];
-            if(prefixSum>=totalSum-prefixSum){
+        for (int i = 0; i < n - 1; i++) {
+            prefixSum += arr[i];
+            if (prefixSum >= totalSum - prefixSum) {
                 count++;
             }
         }
