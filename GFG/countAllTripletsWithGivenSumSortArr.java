698263f2d3961ffd66462aff84af33b51374c2dc
Siddharth Singh <siddhu33singh@gmail.com>
2025-01-04 22:42:42 +0530

    Count all triplets with given sum in sorted array(GFG) Day-4

--- GFG/countAllTripletsWithGivenSumSortArr.java
+++ GFG/countAllTripletsWithGivenSumSortArr.java
@@ -2,29 +2,29 @@
 // Count all triplets with given sum in sorted array
 public class countAllTripletsWithGivenSumSortArr {
     public static void main(String[] args) {
-        int[] arr = {-3, -1, -1, 0, 1, 2};
+        int[] arr = { -3, -1, -1, 0, 1, 2 };
         int target = -2;
         int count = 0;
-        for (int i = 0; i < arr.length-2; i++) {
-            int left = i+1; 
-            int right = arr.length-1;
-            while(left < right) {
-                int sum = arr[i]+arr[left]+arr[right];
-                if(sum == target) {
+        for (int i = 0; i < arr.length - 2; i++) {
+            int left = i + 1;
+            int right = arr.length - 1;
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+                if (sum == target) {
                     count++;
-                    int temp = left+1;
-                    while(temp<right && arr[temp]==arr[temp-1] ){
+                    int temp = left + 1;
+                    while (temp < right && arr[temp] == arr[temp - 1]) {
                         count++;
                         temp++;
                     }
                     right--;
-                }else if(sum < target) {
+                } else if (sum < target) {
                     left++;
-                }else{
+                } else {
                     right--;
                 }
             }
         }
         System.out.println(count);
-    }   
+    }
 }
