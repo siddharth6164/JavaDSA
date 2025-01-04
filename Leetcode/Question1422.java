89e5f1ddd8fcf7347db962a8faf7b61b67e27c5c
Siddharth Singh <siddhu33singh@gmail.com>
2025-01-04 22:50:23 +0530

    1422. Maximum Score After Splitting a String

--- Leetcode/Question1422.java
+++ Leetcode/Question1422.java
@@ -4,22 +4,22 @@ public class Question1422 {
     public static void main(String[] args) {
         String s = "1111";
         int max = 0;
-        for(int i = 1; i < s.length(); i++){
+        for (int i = 1; i < s.length(); i++) {
             int zero = 0;
             int one = 0;
-            for(int j=0;j<i;j++) {
-                if(s.charAt(j)=='0'){
-                    zero=zero+1;
+            for (int j = 0; j < i; j++) {
+                if (s.charAt(j) == '0') {
+                    zero = zero + 1;
                 }
             }
-            for(int k=i; k<s.length(); k++){
-                if(s.charAt(k)=='1'){
-                    one=one+1;
+            for (int k = i; k < s.length(); k++) {
+                if (s.charAt(k) == '1') {
+                    one = one + 1;
                 }
             }
-            int sum=zero+one;
-            if(max<sum){
-                max=sum;
+            int sum = zero + one;
+            if (max < sum) {
+                max = sum;
             }
         }
         System.out.println(max);
