eac9370e40ca598675d5369a4c7f3443090b5dec
Siddharth Singh <siddhu33singh@gmail.com>
2025-01-04 22:44:37 +0530

    Count Subarrays With Given XOR(GFG) Day-3

--- GFG/countSubarraysWithGivenXOR.java
+++ GFG/countSubarraysWithGivenXOR.java
@@ -2,23 +2,24 @@
 // Count Subarrays With Given XOR
 
 import java.util.HashMap;
+
 public class countSubarraysWithGivenXOR {
-    public static void main(String[] args){
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int[] arr = {4,2,2,6,4};
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] arr = { 4, 2, 2, 6, 4 };
         int k = 6;
         int prefixxor = 0;
         int count = 0;
-        for(int i = 0; i < arr.length; i++){
-            prefixxor = prefixxor^arr[i];
-            if(prefixxor==k){
+        for (int i = 0; i < arr.length; i++) {
+            prefixxor = prefixxor ^ arr[i];
+            if (prefixxor == k) {
                 count++;
             }
-            if(map.containsKey((prefixxor^k))){
-                count += map.get(prefixxor^k);
+            if (map.containsKey((prefixxor ^ k))) {
+                count += map.get(prefixxor ^ k);
             }
-            map.put(prefixxor,map.getOrDefault(prefixxor,0)+1);
-        } 
+            map.put(prefixxor, map.getOrDefault(prefixxor, 0) + 1);
+        }
         System.out.println(count);
     }
 }
