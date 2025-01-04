c82475785e86e35352237862ff4054fdc3b789c7
Siddharth Singh <siddhu33singh@gmail.com>
2025-01-04 22:47:36 +0530

    Sub-Arrays With Sum K(GFG) Day-2

--- GFG/subarraysWithSumK.java
+++ GFG/subarraysWithSumK.java
@@ -1,22 +1,24 @@
+
 // GFG
 // Sub-Arrays With Sum K
 import java.util.HashMap;
+
 public class subarraysWithSumK {
     public static void main(String[] args) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int[] arr = {10,2,-2,-20,10};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] arr = { 10, 2, -2, -20, 10 };
         int k = -10;
-        int sum=0;
-        int count=0;
-        for (int i=0; i<arr.length; i++) {
-            sum+=arr[i];
-            if(sum==k){
-                count+=1;
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum == k) {
+                count += 1;
             }
-            if(map.containsKey(sum-k)){
-                count+=map.get(sum-k);
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
             }
-            map.put(sum,map.getOrDefault(sum,0)+1);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         System.out.println(count);
     }
