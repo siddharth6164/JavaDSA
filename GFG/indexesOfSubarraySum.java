ee2320f825453faaeb100707b3bacce3d72848a9
Siddharth Singh <siddhu33singh@gmail.com>
2025-01-09 19:54:40 +0530

    Indexes of Subarray Sum(GFG) Day-9

--- GFG/indexesOfSubarraySum.java
+++ GFG/indexesOfSubarraySum.java
@@ -3,18 +3,18 @@
 import java.util.ArrayList;
 
 public class indexesOfSubarraySum {
-    public static ArrayList<Integer> subArraySum(int[] arr,int target){
+    public static ArrayList<Integer> subArraySum(int[] arr, int target) {
         ArrayList<Integer> result = new ArrayList<Integer>();
-        for(int i=0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             int sum = 0;
-            for(int j=i;j<arr.length;j++){
-                sum+=arr[j];
-                if(sum==target){
-                    result.add(i+1);
-                    result.add(j+1);
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if (sum == target) {
+                    result.add(i + 1);
+                    result.add(j + 1);
                     return result;
                 }
-                if(sum>target){
+                if (sum > target) {
                     break;
                 }
             }
@@ -22,8 +22,9 @@ public class indexesOfSubarraySum {
         result.add(-1);
         return result;
     }
+
     public static void main(String[] args) {
-        int[] arr = {1,2,3,7,5};
+        int[] arr = { 1, 2, 3, 7, 5 };
         int target = 12;
         ArrayList<Integer> ans = subArraySum(arr, target);
         System.out.println(ans);
