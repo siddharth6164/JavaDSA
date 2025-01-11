569e0621ae578668e8f72d2d439e205d04f876ec
Siddharth Singh <siddhu33singh@gmail.com>
2025-01-11 23:04:54 +0530

    Longest substring with distinct characters(GFG) Day-11

---
+++ GFG/longestSubstringWithDistinctCharacters.java
@@ -0,0 +1,28 @@
+public class longestSubstringWithDistinctCharacters {
+    public static int distinctCharcter(String s) {
+        int n = s.length();
+        int[] freq = new int[26];
+        int i = 0, j = 0,count = 0;
+        while(j<n){
+            int ch = s.charAt(j)-'a';
+            if(freq[ch]>0){
+                count = Math.max(count,j-i);
+                while(i<j && s.charAt(i)!=s.charAt(j)){
+                    freq[s.charAt(i)-'a'] = 0;
+                    i++;
+                }
+                i++;
+            }else{
+                freq[ch]++;
+            }
+            j++;
+        }
+        count = Math.max(count,j-i);
+        return count;
+    }
+    public static void main(String[] args) {
+        String s = "geeksforgeeks";
+        int result = distinctCharcter(s);
+        System.out.println(result);
+    }
+}
