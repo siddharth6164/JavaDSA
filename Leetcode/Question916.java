bdfc2dd51145a1d01c52909d5a49be09d694159d
Siddharth Singh <siddhu33singh@gmail.com>
2025-01-18 23:30:50 +0530

    916. Word Subsets

--- Leetcode/Question916.java
+++ Leetcode/Question916.java
@@ -1,3 +1,4 @@
+
 // leetcode
 // 916. Word Subsets
 import java.util.ArrayList;
@@ -6,14 +7,14 @@ import java.util.List;
 public class Question916 {
     public static List<String> wordSubsets(String[] A, String[] B) {
         int[] bmax = count("");
-        for (String b: B) {
+        for (String b : B) {
             int[] bCount = count(b);
             for (int i = 0; i < 26; ++i)
                 bmax[i] = Math.max(bmax[i], bCount[i]);
         }
 
         List<String> ans = new ArrayList();
-        search: for (String a: A) {
+        search: for (String a : A) {
             int[] aCount = count(a);
             for (int i = 0; i < 26; ++i)
                 if (aCount[i] < bmax[i])
@@ -26,13 +27,14 @@ public class Question916 {
 
     public static int[] count(String S) {
         int[] ans = new int[26];
-        for (char c: S.toCharArray())
+        for (char c : S.toCharArray())
             ans[c - 'a']++;
         return ans;
     }
+
     public static void main(String[] args) {
-        String[] words1 = {"amazon","apple","facebook","google","leetcode"};
-        String[] words2 = {"e","o"};
+        String[] words1 = { "amazon", "apple", "facebook", "google", "leetcode" };
+        String[] words2 = { "e", "o" };
         List<String> ans = wordSubsets(words1, words2);
         System.out.println(ans);
     }
