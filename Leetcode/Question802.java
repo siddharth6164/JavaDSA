e1caed42d5366e1850bca5026a53a029f0c24a44
Siddharth Singh <siddhu33singh@gmail.com>
2025-01-24 22:06:47 +0530

    802. Find Eventual Safe States

--- Leetcode/Question802.java
+++ Leetcode/Question802.java
@@ -6,48 +6,49 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Question802 {
+
     private static boolean[] safeNodes;
+
     private static boolean checkSafeState(List<List<Integer>> graph, int[] visited, int curr) {
-        visited[curr] = 1; 
+        visited[curr] = 1;
         boolean isSafe = true;
         for (int neighbor : graph.get(curr)) {
-            if (visited[neighbor] == 0) { 
-                            isSafe &= checkSafeState(graph, visited, neighbor);
-                        } else if (visited[neighbor] == 1) { 
-                            safeNodes[curr] = false;
-                            return false;
-                        } else if (visited[neighbor] == 2) { 
-                            isSafe &= safeNodes[neighbor];
-                        }
-                    }
-            
-                    visited[curr] = 2; 
-                    safeNodes[curr] = isSafe;
-                    return isSafe;
-                }
-            
-                public static List<Integer> eventualSafeNodes(int[][] graph) {
-                    int n = graph.length;
-                    safeNodes = new boolean[n]; 
-                Arrays.fill(safeNodes, true); 
-        
-                int[] visited = new int[n]; 
-        
-                List<List<Integer>> graphList = new ArrayList<>();
-                for (int i = 0; i < n; i++) {
-                    List<Integer> neighbors = new ArrayList<>();
-                    for (int neighbor : graph[i]) {
-                        neighbors.add(neighbor);
-                    }
-                    graphList.add(neighbors);
-                }
-        
-                for (int i = 0; i < n; i++) {
-                    if (visited[i] == 0) {
-                        checkSafeState(graphList, visited, i);
+            if (visited[neighbor] == 0) {
+                isSafe &= checkSafeState(graph, visited, neighbor);
+            } else if (visited[neighbor] == 1) {
+                safeNodes[curr] = false;
+                return false;
+            } else if (visited[neighbor] == 2) {
+                isSafe &= safeNodes[neighbor];
             }
         }
 
+        visited[curr] = 2;
+        safeNodes[curr] = isSafe;
+        return isSafe;
+    }
+
+    public static List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        safeNodes = new boolean[n];
+        Arrays.fill(safeNodes, true);
+
+        int[] visited = new int[n];
+
+        List<List<Integer>> graphList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            List<Integer> neighbors = new ArrayList<>();
+            for (int neighbor : graph[i]) {
+                neighbors.add(neighbor);
+            }
+            graphList.add(neighbors);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 0) {
+                checkSafeState(graphList, visited, i);
+            }
+        }
         List<Integer> result = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             if (safeNodes[i]) {
@@ -56,18 +57,10 @@ public class Question802 {
         }
         return result;
     }
- public static void main(String[] args) {
-    int[][] graph = {
-        {1, 2}, 
-        {2, 3}, 
-        {5}, 
-        {0}, 
-        {5}, 
-        {}, 
-        {}
-    };
 
-    List<Integer> safeNodes = eventualSafeNodes(graph);
-    System.out.println(safeNodes);
- }   
+    public static void main(String[] args) {
+        int[][] graph = {{1, 2}, {2, 3}, {5}, {0},{5}, {}, {}};
+        List<Integer> safeNodes = eventualSafeNodes(graph);
+        System.out.println(safeNodes);
+    }
 }
