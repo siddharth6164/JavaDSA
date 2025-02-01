4701bcc75c1778ecada4cc4b75f4a4df062858b1
Siddharth Singh <siddhu33singh@gmail.com>
2025-02-01 23:49:20 +0530

    Word Search(GFG) Day-32

---
+++ GFG/WordSearch.java
@@ -0,0 +1,45 @@
+// GFG
+// Word Search
+
+public class WordSearch {
+    static public boolean isWordExist(char[][] mat, String word) {
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j] == word.charAt(0)){
+                    boolean[][] visit = new boolean[mat.length][mat[0].length];
+                    if(check(mat, i, j, 0, word, visit))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+    static boolean check(char[][] mat, int i, int j, int ind, String word, boolean[][] visit){
+        if(ind == word.length()){
+            return true;
+        }
+        if(i<0 || j<0 || i>=mat.length || j>= mat[0].length || visit[i][j] || mat[i][j] != word.charAt(ind)){
+            return false;
+        }
+        visit[i][j]=true;
+        if(check(mat, i, j-1, ind+1, word, visit) ||
+           check(mat, i, j+1, ind+1, word, visit) ||
+           check(mat, i+1, j, ind+1, word, visit) ||
+           check(mat, i-1, j, ind+1, word, visit))
+           return true;
+           
+        visit[i][j]=false;
+        return false;
+    }
+    public static void main(String[] args) {
+        char[][] mat = {
+            {'T', 'E', 'E'},
+            {'S', 'G', 'K'},
+            {'T', 'E', 'L'}
+        };
+        String word = "GEEK";
+        boolean result = isWordExist(mat, word);
+        System.out.println("Word exists: " + result);
+
+    }
+}
