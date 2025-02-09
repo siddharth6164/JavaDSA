f90dd3cb2d304eea935d27b7a187ef5c7141b8c8
Siddharth Singh <siddhu33singh@gmail.com>
2025-02-10 00:12:49 +0530

    2364. Count Number of Bad Pairs

---
+++ Leetcode/Question2364.java
@@ -0,0 +1,24 @@
+// leetcode
+// 2364. Count Number of Bad Pairs
+public class Question2364 {
+    public static long countBadPairs(int[] nums){
+        long n = nums.length;
+        HashMap<Integer, Integer>freq = new HashMap<>();
+        long goodPairs = 0;
+
+        for(int i=0;i<n;i++){
+            int key = nums[i]-i;
+            if(freq.containsKey(key)){
+                goodPairs += freq.get(key);
+            }
+            freq.put(key, freq.getOrDefault(key, 0) +1);
+        }
+        long badPairs = n * (n-1)/2 - goodPairs;
+        return badPairs;
+    }
+    public static void main(String[] args) {
+        int[] nums = {4,1,3,3};
+        long res = countBadPairs(nums);
+        System.out.println(res);
+    }
+}
