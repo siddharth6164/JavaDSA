453e048295499a23255e72c3c32de69638153b8c
Siddharth Singh <siddhu33singh@gmail.com>
2025-04-18 23:22:30 +0530

    88. Merge Sorted Array

--- Leetcode/Question88.java
+++ Leetcode/Question88.java
@@ -2,16 +2,38 @@
 import java.util.Arrays;
 
 public class Question88 {
-    public static void main(String[] args) {
-        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
-        int m = 3;
-        int[] nums2 = { 2, 5, 6 };
-        int n = 3;
+    public static void solution1(int[] nums1 ,int m, int[] nums2, int n) {
         for (int j = 0, i = m; j < n; j++) {
             nums1[i] = nums2[j];
             i++;
         }
         Arrays.sort(nums1);
         System.out.println(Arrays.toString(nums1));
+    }
+    public static void solution2(int[] nums1 ,int m, int[] nums2, int n) {
+        int midx = m - 1;
+        int nidx = n - 1;
+        int right = m + n - 1;
+
+        while (nidx >= 0) {
+            if (midx >= 0 && nums1[midx] > nums2[nidx]) {
+                nums1[right] = nums1[midx];
+                midx--;
+            } else {
+                nums1[right] = nums2[nidx];
+                nidx--;
+            }
+            right--;
+        }        
+        System.out.println(Arrays.toString(nums1));
+
+    }
+    public static void main(String[] args) {
+        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int m = 3;
+        int[] nums2 = { 2, 5, 6 };
+        int n = 3;
+        // solution1(nums1, m, nums2, n);
+        solution2(nums1, m, nums2, n);
     }
 }
