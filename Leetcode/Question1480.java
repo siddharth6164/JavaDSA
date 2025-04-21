7d22815877cafcd5f165efa077521b290d14e480
Siddharth Singh <siddhu33singh@gmail.com>
2025-04-22 01:44:19 +0530

    1480. Running Sum of 1d Array

--- Leetcode/Question1480.java
+++ Leetcode/Question1480.java
@@ -1,6 +1,16 @@
 // 1480. Running Sum of 1d Array
+
+import java.util.Arrays;
+
 public class Question1480 {
     public static void main(String[] args) {
-        int[]
+        int[] nums = {3,1,2,10,1};
+        int[] arr = new int[nums.length];
+        int ans =0;
+        for(int i=0;i<nums.length;i++){
+            ans+=nums[i];
+            arr[i]=ans;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
