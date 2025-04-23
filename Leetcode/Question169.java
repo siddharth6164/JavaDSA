8653e499c97ce51d74d8465eb1add270257d8a98
Siddharth Singh <siddhu33singh@gmail.com>
2025-04-24 00:21:30 +0530

    169. Majority Element

--- Leetcode/Question169.java
+++ Leetcode/Question169.java
@@ -1,6 +1,20 @@
 // 169. Majority Element
 public class Question169 {
     public static void main(String[] args) {
-        
+        int[] arr = {2,2,1,1,1,2,2};
+        int candidate = arr[0];
+        int count = 0;
+        for(int i : arr){
+            if(i==candidate){
+                count++;
+            }else{
+                count--;
+                if(count==0){
+                    candidate=i;
+                    count++;
+                }
+            }
+        }
+        System.out.println(candidate+" "+count);
     }
 }
