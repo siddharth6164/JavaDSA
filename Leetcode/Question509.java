cd5ac2f8569487f6596f50294c229fc8ef8ee8c8
Siddharth Singh <siddhu33singh@gmail.com>
2025-04-24 23:43:09 +0530

    509. Fibonacci Number

--- Leetcode/Question509.java
+++ Leetcode/Question509.java
@@ -1,13 +1,13 @@
 public class Question509 {
-    public static int soulation(int n){
+    public static int soulation1(int n){
         if(n==0||n==1){
             return n;
         }
-        int sum = soulation(n-1)+soulation(n-2);
+        int sum = soulation1(n-1)+soulation1(n-2);
         return sum;
     }
     public static void main(String[] args) {
         int n = 4;
-        System.out.println(soulation(n));
+        System.out.println(soulation1(n));
     }
 }
---
+++ a.js
@@ -0,0 +1,4 @@
+function aa(a,b,c){
+    console.log(arguments[0],arguments[1],arguments[2])
+}
+aa(10,20,30)
