e9c2c2110c8969520e3acf9bb9e6f1e75b88de54
Siddharth Singh <siddhu33singh@gmail.com>
2025-04-24 23:55:29 +0530

    509. Fibonacci Number

--- Leetcode/Question509.java
+++ Leetcode/Question509.java
@@ -1,4 +1,17 @@
+// 509. Fibonacci Number
 public class Question509 {
+    public static int soulation2(int n){
+        if(n==0|| n==1){
+            return n;
+        }
+        int [] data = new int[n+1];
+        data[0] = 0;
+        data[1] = 1;
+        for(int i=2;i<=n;i++){
+            data[i] = data[i-1] + data[i-2];
+        }
+        return data[n];
+    }
     public static int soulation1(int n){
         if(n==0||n==1){
             return n;
@@ -7,7 +20,10 @@ public class Question509 {
         return sum;
     }
     public static void main(String[] args) {
-        int n = 4;
+        int n = 5;
         System.out.println(soulation1(n));
+        // O(n)
+        System.out.println(soulation2(n));
+
     }
 }
