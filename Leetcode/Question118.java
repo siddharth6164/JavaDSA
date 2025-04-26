c2d504b2861079548c39d385a0c05c833d72ea7d
Siddharth Singh <siddhu33singh@gmail.com>
2025-04-26 23:43:13 +0530

    118. Pascal's Triangle

---
+++ Leetcode/Question118.java
@@ -0,0 +1,16 @@
+// 118. Pascal's Triangle
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question118 {
+    public static List<List<Integer>> solution(int n){
+        List<List<Integer>> arr = new ArrayList<>();
+
+        return arr;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        List<List<Integer>> arr = solution(n);
+        System.out.println(arr);
+    }
+}
