7a508f045ca7ce58d3892641c4cc7dbff1b7d4f7
Siddharth Singh <siddhu33singh@gmail.com>
2025-04-29 21:21:58 +0530

    15. 3Sum

--- Leetcode/Question15.java
+++ Leetcode/Question15.java
@@ -1,23 +1,11 @@
-// Leetcode
 // 15. 3Sum
-// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
-
-// Case1
-// Input: nums = [-1,0,1,2,-1,-4]
-// Output: [[-1,-1,2],[-1,0,1]]
-// Explanation: 
-// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
-// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
-// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
-// The distinct triplets are [-1,0,1] and [-1,-1,2].
-// Notice that the order of the output and the order of the triplets does not matter.
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Question15 {
-    static List<List<Integer>> solution(int[] nums){
+    static List<List<Integer>> solution(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 2; i++) {
@@ -60,9 +48,10 @@ public class Question15 {
         }
         return ans;
     }
+
     public static void main(String[] args) {
-        int[] nums = {-1,0,1,2,-1,-4};
+        int[] nums = { -1, 0, 1, 2, -1, -4 };
         List<List<Integer>> ans = solution(nums);
-        System.out.println(ans);        
+        System.out.println(ans);
     }
 }
