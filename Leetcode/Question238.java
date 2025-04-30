becd1ebefdcf597d9adfc172f33b8838c7065407
Siddharth Singh <siddhu33singh@gmail.com>
2025-05-01 00:58:02 +0530

    238. Product of Array Except Self

--- Leetcode/Question238.java
+++ Leetcode/Question238.java
@@ -1,6 +1,30 @@
 // 238. Product of Array Except Self
+
+import java.util.Arrays;
+
 public class Question238 {
+    public static int[] solution1(int[] nums){
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        left[0]=1;
+        for(int i=1;i<nums.length;i++){
+            left[i]=left[i-1]*nums[i-1];
+        }
+        
+        right[nums.length-1]=1;
+        for(int i=nums.length-2;i>-1;i--){
+            right[i]=right[i+1]*nums[i+1];
+        }
+        
+        int[] ans = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=left[i]*right[i];
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int[] nums = {1,2,3,4};
+        int[] output = solution1(nums);
+        System.out.println(Arrays.toString(output));
     }
 }
