4e44bfe9ddf099e3cf68ddff3700a5b185ac7c63
Siddharth Singh <siddhu33singh@gmail.com>
2025-06-03 23:03:20 +0530

    Overriding

---
+++ JavaBasic/Polymorphism/Overriding.java
@@ -0,0 +1,9 @@
+// Method overriding occurs when a subclass defines a method with the same name, return type, 
+// and parameters as a method in its superclass, but with a different implementation.
+
+
+package Polymorphism;
+
+public class Overriding {
+
+}
