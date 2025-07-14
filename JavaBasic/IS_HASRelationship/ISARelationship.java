5a33cb930ad75e8c07872fd8ae143cfa4c234753
Siddharth Singh <siddhu33singh@gmail.com>
2025-07-14 15:05:27 +0530

    IS/HAS A Relationship

--- JavaBasic/IS_HASRelationship/ISARelationship.java
+++ JavaBasic/IS_HASRelationship/ISARelationship.java
@@ -1,16 +1,17 @@
 package IS_HASRelationship;
 
-class Animal{
-    void makeSound(){
+class Animal {
+    void makeSound() {
         System.out.println("Animal makes a sound");
     }
 }
 
-class Dog extends Animal{
-    void bark(){
+class Dog extends Animal {
+    void bark() {
         System.out.println("Dog barks");
     }
 }
+
 public class ISARelationship {
     public static void main(String[] args) {
         Animal a = new Dog();
